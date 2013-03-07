d41b7a3ab76490645ff2d2d896371585e7e639fa
Toni Petrovic <toni.petrovic@gmail.com>
2013-03-07 09:57:09 +0100

    fixing navigation

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -2,32 +2,36 @@ $(function() {
 
 
 
-    $("#my-cart").fixPosition($(".show-cart"));
+    $("#my-cart").fixPosition($(".cart-toggle"));
 
     var hideMyCart = function(){
-        $(".show-cart").parent('li').removeClass('active');
+        $(".cart-toggle").parent('li').removeClass('open');
         $("#my-cart").removeClass('visi');    
     }
 
     var showMyCart = function(){
-        $(".show-cart").parent('li').addClass('active');
+        $(".cart-toggle").parent('li').addClass('open');
         $("#my-cart").addClass('visi');    
     }
 
-    $(".show-cart").on("click", function(event){
+    $(".cart-toggle").on("click", function(event){
         event.preventDefault();
-        $(this).parent('li').toggleClass('active');
+        $(this).parent('li').toggleClass('open');
         $("#my-cart").fixPosition($(this)).toggleClass('visi');
     });
 
     $("#my-cart a.close").on("click", function(event){
         event.preventDefault();
-        $(".show-cart").parent('li').removeClass('active');
+        $(".cart-toggle").parent('li').removeClass('open');
         $("#my-cart").removeClass('visi');
     });
 
     $(window).resize(function() {
-        $("#my-cart").fixPosition($(".show-cart"));
+        $("#my-cart").fixPosition($(".cart-toggle"));
+    });
+
+    $(window).scroll(function() {
+        $("#my-cart").fixPosition($(".cart-toggle"));
     });
 
     $('.dropdown-toggle').click(function () {
@@ -53,7 +57,7 @@ $(function() {
 
         if(visibility != 'visible'){
             // console.log('remove');
-            $(".show-cart").parent('li').removeClass('active');
+            $(".cart-toggle").parent('li').removeClass('open');
             $('#my-cart').removeClass('visi');
         }
     });
@@ -66,9 +70,10 @@ jQuery.fn.fixPosition = function(element) {
 
     var offset = element.offset();
     var offsetCorrection = 10;
+    var verticalOffsetCorrection = 1;
     var heightOffset = element.outerHeight();
 
-    this.css({top: offset.top + heightOffset});
+    this.css({top: offset.top + heightOffset + verticalOffsetCorrection});
 
     if(offset.left < windowHalf){
         this.css({left: offset.left - offsetCorrection});
