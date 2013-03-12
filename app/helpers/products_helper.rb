a6bc904843f6fd288256491c73066b75426ff387
Toni Petrovic <toni.petrovic@gmail.com>
2013-03-12 18:00:12 +0100

    css work

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -12,4 +12,17 @@ module ProductsHelper
         end
     end
 
+    def product_thumbanil_tag(product, options = {})
+        options[:size] ||= "300x200"
+
+        picture = product.default_picture_id ? product.pictures.find(product.default_picture_id) : product.pictures.first
+
+        if picture
+            content_tag(:div, content_tag(:span, ''), :class => 'thumbnail default-picture', :style => "background-image: url(#{picture.image_url(:thumb)})")
+            #image_tag(picture.image_url(:thumb), :alt => picture.name)
+        else
+            image_tag("http://placehold.it/#{options[:size]}", :size => options[:size])
+        end
+    end
+
 end
